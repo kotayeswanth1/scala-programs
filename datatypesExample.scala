ccbda665c8176e1901d601a0734ae063f08976c1
kotayeswanth1 <102663888+kotayeswanth1@users.noreply.github.com>
2023-03-03 16:18:29 +0530

    Add files via upload

---
+++ datatypesExample.scala
@@ -0,0 +1,30 @@
+object datatypesExample{
+def main(args:Array[String])={
+
+var a=args(0)
+println(a)
+
+var b=args(1)
+println(b)
+
+var c=args(2)
+println(c)
+
+
+var i:Short=32755
+println(i)
+
+var j:Int=2147483645
+println(j)
+
+var k:Long=922337203685477500l
+println(k)
+
+var l:Float=32.275525555022551f
+println(l)
+
+var m:Double=32.5555555555555555555555555555555555555555555555
+println(m)
+
+}
+}
---
+++ whileExample.scala
@@ -0,0 +1,11 @@
+object whileExample{
+def main(args:Array[String])={
+var i=10;
+while(i<20)
+{
+println("i ="+i)
+i=i+1;
+}
+
+}
+}
